ff9c957ef57136a4fb069b7ad9c8bbbfac5b42ae
andreirhamni09 <andreirhamni09@gmail.com>
2022-02-12 19:55:40 +0700

    Mantap

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,3 @@
-<?php
- session_start();
-?>
 <!DOCTYPE html>
 <html>
 
@@ -83,9 +80,8 @@
         </nav>
         <aside class="main-sidebar sidebar-light-primary elevation-4">
             <a href="{{ url('/') }}" class="brand-link">
-                <img src="{{ asset('public/img/CBI-logo.png') }}" alt="Covid Tracker"
-                    class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">IoT</span>
+                <img src="" alt="Covid Tracker" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">Manajemen Truck</span>
             </a>
             <div class="sidebar">
                 <nav class="mt-2">
